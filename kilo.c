805212cb651033051235f2f9ac16f9067e166a74
deadmeu <deadmeu@gmail.com>
2019-08-14 15:40:25 +1000

    Hide cursor during refresh

--- kilo.c
+++ kilo.c
@@ -124,12 +124,16 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
+    // Hide the cursor
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    // Show the cursor
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
