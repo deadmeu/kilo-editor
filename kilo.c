36c236d4df8e1a2bc18110f18d739d01e10131e4
deadmeu <deadmeu@gmail.com>
2019-08-14 17:16:51 +1000

    Added home & end keys

--- kilo.c
+++ kilo.c
@@ -18,6 +18,10 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** data ***/
@@ -74,11 +78,32 @@ int editorReadKey() {
         if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
         if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '1':
+                        case '7': return HOME_KEY;
+                        case '4':
+                        case '8': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                    }
+                }
+            }
+
             switch (seq[1]) {
                 case 'A': return ARROW_UP;
                 case 'B': return ARROW_DOWN;
                 case 'C': return ARROW_RIGHT;
                 case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+            }
+        } else if (seq[0] == 'O') {
+            switch (seq[1]) {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
 
@@ -223,6 +248,20 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+
+        // Home and end, start/end of line snapping
+        case HOME_KEY: E.cx = 0; break;
+        case END_KEY: E.cx = E.screencols - 1; break;
+
+        // Page up/down cursor snapping
+        case PAGE_UP:
+        case PAGE_DOWN: {
+            for (int times = E.screenrows; times > 0; times--) {
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
+        }
+
         // Cursor movement keys
         case ARROW_UP:
         case ARROW_LEFT:
