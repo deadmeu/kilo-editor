f8d884b5d692bb081b76bdf09750b0c004fa8de4
deadmeu <deadmeu@gmail.com>
2019-08-17 18:10:53 +1000

    Added scrolling

--- kilo.c
+++ kilo.c
@@ -38,6 +38,7 @@ typedef struct erow {
 
 struct editorConfig {
     int cx, cy;
+    int rowoff;
     int screenrows;
     int screencols;
     int numrows;
@@ -213,9 +214,16 @@ void abAppend(struct abuf *ab, const char *s, int len) {
 void abFree(struct abuf *ab) { free(ab->b); }
 
 /*** output ***/
+void editorScroll() {
+    E.rowoff = (E.cy < E.rowoff) ? E.cy : E.rowoff;
+    E.rowoff =
+        (E.cy >= E.rowoff + E.screenrows) ? E.cy - E.screenrows + 1 : E.rowoff;
+}
+
 void editorDrawRows(struct abuf *ab) {
     for (int y = 0; y < E.screenrows; y++) {
-        if (y >= E.numrows) {
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows) {
             // Draw welcome message
             if (E.numrows == 0 && y == E.screenrows / 3) {
                 char welcome[80];
@@ -236,9 +244,9 @@ void editorDrawRows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row[y].size;
+            int len = E.row[filerow].size;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[y].chars, len);
+            abAppend(ab, E.row[filerow].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -249,6 +257,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     // Hide the cursor
@@ -260,7 +270,7 @@ void editorRefreshScreen() {
 
     // Move the cursor
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
     abAppend(&ab, buf, strlen(buf));
 
     // Show the cursor
@@ -288,7 +298,7 @@ void editorMoveCursor(int key) {
             break;
         case ARROW_DOWN:
             E.cy++;
-            E.cy = (E.cy >= E.screenrows) ? E.screenrows - 1 : E.cy;
+            E.cy = (E.cy >= E.numrows) ? E.numrows - 1 : E.cy;
             break;
     }
 }
@@ -329,6 +339,7 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rowoff = 0;
     E.numrows = 0;
     E.row = NULL;
 
