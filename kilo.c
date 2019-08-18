f9ad85a824970a5e7253007c4378017b925b80c1
deadmeu <deadmeu@gmail.com>
2019-08-19 00:03:03 +1000

    Horizontal scrolling limited to right

--- kilo.c
+++ kilo.c
@@ -301,13 +301,17 @@ void editorRefreshScreen() {
 
 /*** input ***/
 void editorMoveCursor(int key) {
+    erow *row = (E.cy < E.numrows) ? &E.row[E.cy] : NULL;
+
     switch (key) {
         // Cursor position values clamped between 0 and screen dimensions
         case ARROW_LEFT:
             E.cx--;
             E.cx = (E.cx < 0) ? 0 : E.cx;
             break;
-        case ARROW_RIGHT: E.cx++; break;
+        case ARROW_RIGHT:
+            if (row && E.cx < row->size) E.cx++;
+            break;
         case ARROW_UP:
             E.cy--;
             E.cy = (E.cy < 0) ? 0 : E.cy;
